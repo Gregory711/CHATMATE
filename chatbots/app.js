a9d0b0016d9673032599ca76995e59633a97a712
Gregory <gregorytravis711@gmail.com>
2023-04-27 23:43:24 -0400

    Added request npm dependency and added chat function to server to get local model response and added endpoint for testing

--- chatbots/app.js
+++ chatbots/app.js
@@ -2,7 +2,58 @@ const express = require('express');
 const app = express();
 require("dotenv").config();
 const port = process.env.CHATBOTSPORT || 3000;
+const request = require('request');
+const max_tokens = 50;
 
 app.listen(port, () => {
     console.log(`Chatbots server listening on port ${port}`);
+});
+
+async function chat(contents) {
+    var options;
+
+    if (process.env.MODE === 'local') {
+        options = {
+            url: 'http://localhost:' + process.env.LOCALHOSTPORT + '/v1/chat/completions',
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+              model: process.env.LOCALHOSTMODEL,
+              messages: [
+                {
+                  role: 'user',
+                  content: contents
+                }
+              ],
+              temperature: 0,
+              max_tokens: max_tokens
+            })
+          };
+    }
+
+    return new Promise(function (resolve, reject) {
+        request(options, (error, response, body) => {
+            if (error) {
+                console.error('Error:', error);
+                return reject(error);
+            }
+            try {
+                resolve(JSON.parse(body).choices[0].message.content);
+            } catch (error) {
+                reject(error);
+            }
+        });
+    });
+}
+
+app.get('/test', async (req, res) => {
+    try {
+        const message = await chat("If I have one apple and you have two apples, how many apples do we have together?");
+        console.log(message);
+        res.send(message);
+    } catch (error) {
+        res.send(500, error);
+    }
 });
