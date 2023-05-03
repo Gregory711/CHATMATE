acf3d941918deb2892cdf8344bd5687896fc397f
Gregory Travis <gregorytravis711@gmail.com>
2023-05-03 19:24:06 -0400

    Updated list-chatbots.js

--- chatbots/prompts/list-chatbots.js
+++ chatbots/prompts/list-chatbots.js
@@ -1,9 +1,9 @@
-const fs = require('fs');
+import fs from 'fs';
 
 // Read the metadata file
 const metadata = JSON.parse(fs.readFileSync('./chatbots/prompts/metadata.json'));
 
 // Iterate over the chatbots and print their names
 for (const chatbot of metadata) {
-    console.log(chatbot[1]);
+    console.log(chatbot[0]);
 }
