df34197e2caf186c955a7b4edfb0f886605c05ae
Gregory Travis <gregorytravis711@gmail.com>
2023-05-04 14:36:35 -0400

    Updated create-prompt CLI to create empty response arrays and added empty response arrays for Cartoon Villain

--- chatbots/prompts/create-prompt.js
+++ chatbots/prompts/create-prompt.js
@@ -8,3 +8,8 @@ prompts[process.argv[2]].push(process.argv[3]);
 
 // Write the prompts file
 fs.writeFileSync('./chatbots/prompts/prompts.json', JSON.stringify(prompts));
+
+// Add empty responses array for the new prompt
+const responses = JSON.parse(fs.readFileSync('./chatbots/responses/responses.json'));
+responses[process.argv[2]].push([]);
+fs.writeFileSync('./chatbots/responses/responses.json', JSON.stringify(responses));
